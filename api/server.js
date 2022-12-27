cb1594a0935506fb086666cc45ea80060a2fdae3
John Zhu <johnszhu@live.unc.edu>
2022-12-27 16:29:41 -0500

    started backend development

--- api/server.js
+++ api/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
-
-
 const app = express();
-const port = process.env.PORT || 3080;
+const port = process.env.PORT || 3000;
+
+/* Router Handlers */
 
 
+app.get('/', (req,res) => {
+    res.send("Hello World")
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
